7bc5d6c2c140581cc8905046b4ea09a831a98795
Radnet12 <sergeyvasylenko4@gmail.com>
2022-01-06 16:13:38 +0200

    added User model

--- back/index.js
+++ back/index.js
@@ -1,8 +1,20 @@
 // Modules
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 5001;
 const app = express();
 
-app.listen(PORT, () => `Сервер запущен! Порт ${PORT}`);
+const startServer = async () => {
+    try {
+        // Connection to DB
+        await mongoose.connect(process.env.DB_CONNECTION_URL);
+
+        app.listen(PORT, () => console.log(`Сервер запущен! Порт ${PORT}`));
+    } catch (e) {
+        console.log("Ошибка при запуске сервера! Ошибка: ", e);
+    }
+};
+
+startServer();
